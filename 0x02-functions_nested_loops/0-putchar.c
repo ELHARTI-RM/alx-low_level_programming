24b5519ae99aad9b18aeb11459bf73036c74790b
ELHARTI-RM <elharti.abdessamad99@gmail.com>
2023-03-21 17:45:01 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main- Entry Point
+ * Description A program that prints _putchar, followed by a new line
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
-

634cdee81cd2e9e969edba34b72fa7676c2596c0
ELHARTI-RM <elharti.abdessamad99@gmail.com>
2023-04-01 08:01:04 -0700

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,13 +3,15 @@
 #include <time.h>
 
 /**
- * main - program generate random password
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int passw[100], i, sum, n;
+	int pass[100];
+	int i, sum, n;
 
 	sum = 0;
 
@@ -17,9 +19,9 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		passw[i] = rand() % 78;
-		sum += (passw[i] + '0');
-		putchar(passw[i] + '0');
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
@@ -30,4 +32,4 @@ int main(void)
 	}
 
 	return (0);
-}}
+}

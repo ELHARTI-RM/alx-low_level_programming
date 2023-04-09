fb0be1e0cb6fbf7154a4d3b490a6615e57baf2c4
ELHARTI-RM <elharti.abdessamad99@gmail.com>
2023-04-09 06:08:49 -0700

    end-dest

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,28 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings,
  * @dest: destination
  * @src: source
  * Return: pointer to dest string
- */
+*/
+
 char *_strcat(char *dest, char *src)
 {
     char *end = dest;
+    
     while (*end != '\0')
     {
         end++;
     }
+    
     while (*src != '\0')
     {
         *end = *src;
         end++;
         src++;
     }
+    
     *end = '\0';
     return dest;
 }
